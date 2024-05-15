41020ade697f6c1f0d868a5e6949a2c249eb2d20
hmpark <hmpark@hidea.kr>
2024-05-15 23:11:47 +0900

    makeOne(dp) - edit

--- DP/MakeOne.java
+++ DP/MakeOne.java
@@ -28,9 +28,11 @@ public class MakeOne {
             dp[i] = dp[i - 1] + 1;
             if(i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            } else if (i % 3 == 0) {
+            }
+            if (i % 3 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            } else if (i % 5 == 0) {
+            }
+            if (i % 5 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 5] + 1);
             } 
         }
